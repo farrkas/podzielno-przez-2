1df00e2fefedc700896931320bf982fa3add0653
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-11 00:06:43 +0100

    do naprawy

---
+++ przez 3.js
@@ -0,0 +1,17 @@
+function przeztrzy(a) {
+    if (isNaN(a)) {
+        console.log("wpisana wartość nie jest liczbą");
+    }
+    else {
+        var b = a.substr(a.length - 2, 2);
+        if (b % 3= 0) {
+            console.log("podzielna przez 3");
+        }
+      else {
+            console.log("niepodzielna przez 3");
+        }
+    }
+
+
+}
+przeztrzy(15485412);
