9aeac0477d66578a33aa9cfb67245741b5042b8f
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-11 00:22:57 +0100

    koniec na dziś, nie działa pobranie ostatnich dwóch liczb

--- przez3.js
+++ przez3.js
@@ -1,21 +1,22 @@
-//function przeztrzy(a) {
- //   if (isNaN(a)) {
- //       console.log("wpisana wartość nie jest liczbą");
- //   }
- //   else {
- //       var b = a.substr(a.length - 2, 2);
- //       if (b%3==0) {
- //           console.log("podzielna przez 3");
- //       }
- //     else {
- //           console.log("niepodzielna przez 3");
- //       }
- //   }
+function przeztrzy(a) {
+    if (isNaN(a)) {
+        console.log("wpisana wartość nie jest liczbą");
+    }
+    else {
+        const b=a.length-3;
+        const c = a.slice(b);
+        if (c%3==0) {
+            console.log("podzielna przez 3");
+        }
+      else {
+            console.log("niepodzielna przez 3");
+        }
+    }
 
 
-//}
-//przeztrzy(15485412);
+}
+przeztrzy(15485412);
 
-var a="sadrwe";
+//var a="sadrwe";
 
-console.log(toString.a.substr(a.length-2, 2);)
+//console.log(a.substr(a.length);
