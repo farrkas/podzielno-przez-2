3c051cf682523ae04ae3b9aea3ca9150d22bcebf
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-11 23:51:22 +0100

    skonczone!

--- przez3.js
+++ przez3.js
@@ -3,20 +3,16 @@ function przeztrzy(a) {
         console.log("wpisana wartość nie jest liczbą");
     }
     else {
-        const b=a.length-3;
-        const c = a.slice(b);
-        if (c%3==0) {
+        // const b=a.length-3;
+        // var atab=[...a]
+        // const c = atab.slice(b);
+        if (a % 3 == 0) {
             console.log("podzielna przez 3");
         }
-      else {
+        else {
             console.log("niepodzielna przez 3");
         }
     }
 
-
 }
-przeztrzy(15485412);
-
-//var a="sadrwe";
-
-//console.log(a.substr(a.length);
+przeztrzy(15485411);
