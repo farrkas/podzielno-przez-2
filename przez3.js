061fd8bf5bca16def68b697d455d0d28844aa69a
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-25 20:13:43 +0100

    nie działa jak powinno

--- przez3.js
+++ przez3.js
@@ -1,18 +1,16 @@
 function przeztrzy(a) {
     if (isNaN(a)) {
-        console.log("wpisana wartość nie jest liczbą");
+        console.log("wpisana wartość nie jest liczbą!!!");
     }
     else {
-        // const b=a.length-3;
-        // var atab=[...a]
-        // const c = atab.slice(b);
-        if (a % 3 == 0) {
+        if (a.substr(a.length-2) % 3 == 0) {
             console.log("podzielna przez 3");
         }
         else {
             console.log("niepodzielna przez 3");
+
         }
     }
 
 }
-przeztrzy(15485411);
+przeztrzy(1111111115);
